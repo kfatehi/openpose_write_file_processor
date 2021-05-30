88351cc2ab920498168317cba8c2d93a7cec4b2e
Keyvan Fatehi <keyvanfatehi@gmail.com>
2021-05-30 13:52:57 -0700

    something to test

--- handle-keypoints.js
+++ handle-keypoints.js
@@ -1,12 +1,16 @@
 module.exports = function(keypointsContainer, callback) {
     if (keypointsContainer.people.length == 0) return;
-    const keypoints = keypointsContainer.people[0].pose_keypoints_2d;
+    const kp = keypointsContainer.people[0].pose_keypoints_2d;
     // [582.349,507.866,0.845918,746.975,631.307,0.587007,...],
 
-    console.log(keypoints)
-
-    // get shoulders 
-    // get elbows
+    let leftShoulder = {x:kp[5],y:kp[5+1]}
+    let leftElbow = {x:kp[6],y:kp[6+1]}
+    let rightShoulder = {x:kp[2],y:kp[2+1]}
+    let rightElbow = {x:kp[3],y:kp[3+1]}
     
+    console.log({
+        leftShoulderElbowDiffY: leftElbow.y - leftShoulder.y
+    })
+ 
     callback();
 }
